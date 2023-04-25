61f424bb4c1aaedfa45df81879d9e329d88c6570
William Pedro <williamprv@outlook.com>
2023-04-25 20:35:15 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+         //WINDOWS
+using System;
 using System.Diagnostics;
 using System.IO;
 
@@ -6,5 +7,19 @@ class Program {
     static void Main(string[] args) {
         string currentDirectory = Directory.GetCurrentDirectory();
         Process.Start("explorer.exe", currentDirectory);
+    }
+}
+/////////////////////////////////////////////////////////////
+
+        //lINUX
+
+using System;
+using System.Diagnostics;
+using System.IO;
+
+class Program {
+    static void Main(string[] args) {
+        string currentDirectory = Directory.GetCurrentDirectory();
+        Process.Start("nautilus", currentDirectory);
     }
 }
